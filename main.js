24ab3169fa05cf5dec58626c26e89da2d64c3c4f
MehemmedIsmayilov <mehemmedmi@code.edu.az>
2023-03-07 16:03:05 +0400

    period 3

---
+++ main.js
@@ -0,0 +1,27 @@
+let prew = document.querySelector(".prew");
+let next = document.querySelector(".next");
+let img = document.querySelector("img");
+
+let index = 0;
+let arr = ["img1.jpg", "img2.jpg", "img3.jpg"];
+
+next.addEventListener("click", () => {
+  img.setAttribute("src", `./${arr[index]}`);
+
+  if (index == arr.length - 1) {
+    index = 0;
+  } else {
+    index++;
+  }
+});
+
+prew.addEventListener("click",()=>{
+    img.setAttribute("src",`./${arr[index]}`)
+
+    if(index==0){
+        index=arr.length-1;
+    }
+    else{
+        index--;
+    }
+})
